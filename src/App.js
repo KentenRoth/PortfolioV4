ed1a277d3ab464d3e2b49ed696623e8f187e4645
Kent <kentencroth@yahoo.com>
2023-05-28 13:50:53 -0500

    Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './sass/app.sass';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+import About from './Pages/About';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/about-me" element={<About />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
