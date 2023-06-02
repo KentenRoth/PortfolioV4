246c2e24a62aec87a691b84facc3467c258ce318
Kent <kentencroth@yahoo.com>
2023-06-02 17:02:41 -0500

    Project Card

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Nav from '../Components/nav';
+import projects from '../data.json';
+import ProjectCard from '../Components/project-card';
 
 const Projects = () => {
 	return (
 		<div className="projects">
 			<Nav page={'Projects'} />
+			<div className="projects_list">
+				{projects.projects.map((x) => {
+					return <ProjectCard project={x} />;
+				})}
+			</div>
 		</div>
 	);
 };
