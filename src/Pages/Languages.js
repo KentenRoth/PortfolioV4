c2e8707c6885883669e8513c78c82be741d8cb97
Kent <kentencroth@yahoo.com>
2023-06-03 17:46:54 -0500

    Added languages section

---
+++ src/Pages/Languages.js
@@ -0,0 +1,15 @@
+import Rect from 'react';
+
+const Languages = () => {
+	return (
+		<>
+			<div className="languages">
+				<div className="languages_content">
+					<h1>Languages</h1>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Languages;
