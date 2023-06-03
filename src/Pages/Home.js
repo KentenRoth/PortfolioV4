4ca9f093b1088fd67d727d5c49bd43c69717de43
Kent <kentencroth@yahoo.com>
2023-06-03 16:25:51 -0500

    Layout adjustment

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Socials from '../Components/socials';
 import Hero from '../Pages/Hero';
 import Projects from './Projects';
 import About from './About';
@@ -7,9 +6,7 @@ import About from './About';
 const Home = () => {
 	return (
 		<>
-			<section>
-				<Hero />
-			</section>
+			<Hero />
 			<section id="projects">
 				<Projects />
 			</section>
