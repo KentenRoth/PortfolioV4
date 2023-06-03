3eb4121de19c76dcb624cc5dda214e97b7de1abd
Kent <kentencroth@yahoo.com>
2023-06-03 17:47:10 -0500

    added variables

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Hero from '../Pages/Hero';
 import Projects from './Projects';
 import About from './About';
+import Languages from './Languages';
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
-			<section id="projects">
+			<section className="section--projects" id="projects">
 				<Projects />
 			</section>
-			<section id="about">
+			<Languages />
+			<section className="section--about" id="about">
 				<About />
 			</section>
 		</>
