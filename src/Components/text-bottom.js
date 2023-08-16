4d6890475fa0dda1b5786cdbdf50c63d28ec6a5a
Kent <kent@metrostudios.net>
2023-08-16 18:31:05 -0500

    Updated Text

--- src/Components/text-bottom.js
+++ src/Components/text-bottom.js
@@ -2,14 +2,45 @@ import React from 'react';
 
 const TextBottom = () => {
 	return (
-		<div class="cta">
-			<p>
-				VS Code <span class="pipes">-</span> Adobe XD{' '}
-				<span class="pipes">-</span> Photoshop{' '}
-				<span class="pipes">-</span> Communication{' '}
-				<span class="pipes">-</span> Self-Starter{' '}
-				<span class="pipes">-</span> Always Learning{' '}
-			</p>
+		<div class="scrolling-text-container">
+			<div id="outer">
+				<div>
+					<div id="loop">
+						<div id="content">
+							VS Code <span class="pipes">-</span> Adobe XD{' '}
+							<span class="pipes">-</span> Photoshop{' '}
+							<span class="pipes">-</span> Communication{' '}
+							<span class="pipes">-</span> Self-Starter{' '}
+							<span class="pipes">-</span> Always Learning{' '}
+							<span class="pipes">-</span> &nbsp;
+						</div>
+						<div id="content">
+							VS Code <span class="pipes">-</span> Adobe XD{' '}
+							<span class="pipes">-</span> Photoshop{' '}
+							<span class="pipes">-</span> Communication{' '}
+							<span class="pipes">-</span> Self-Starter{' '}
+							<span class="pipes">-</span> Always Learning{' '}
+							<span class="pipes">-</span> &nbsp;
+						</div>
+						<div id="content">
+							VS Code <span class="pipes">-</span> Adobe XD{' '}
+							<span class="pipes">-</span> Photoshop{' '}
+							<span class="pipes">-</span> Communication{' '}
+							<span class="pipes">-</span> Self-Starter{' '}
+							<span class="pipes">-</span> Always Learning{' '}
+							<span class="pipes">-</span> &nbsp;
+						</div>
+						<div id="content">
+							VS Code <span class="pipes">-</span> Adobe XD{' '}
+							<span class="pipes">-</span> Photoshop{' '}
+							<span class="pipes">-</span> Communication{' '}
+							<span class="pipes">-</span> Self-Starter{' '}
+							<span class="pipes">-</span> Always Learning{' '}
+							<span class="pipes">-</span> &nbsp;
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
